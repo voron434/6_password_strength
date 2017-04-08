5b5d56d3b3c6a1909d3b16cbebf16ad38d0ef582
voron434 <070809010@mail.ru>
2017-04-08 18:42:22 +0300

    refactoring into functions

--- password_strength.py
+++ password_strength.py
@@ -1,32 +1,39 @@
-def get_password_strength(password):
-    strength = 1
+def is_symbol_there(password):
     symbols = '#$@'
-    digits = '0123456789'
-    password_blacklist = ['password1', '12345', 'qwerty', 'bestpassword', 'password']
-    if not (password.islower() or password.isupper()):
-        strength += 1
-
-    for digit in digits:
-        if digit in password:
-            strength += 1
-            break
-
     for symbol in symbols:
         if symbol in password:
-            strength += 1
-            break
+            return True
 
+
+def is_digit_there(password):
+    digits = '0123456789'
+    for digit in digits:
+        if digit in password:
+            return True
+
+
+def is_not_from_blacklist(password):
+    password_blacklist = ['password1', '12345', 'qwerty', 'bestpassword', 'password']
     for password_from_blacklist in password_blacklist:
         if password.lower() == password_from_blacklist.lower():
-            break
-    else:
-        strength += 4
+            return False
+    return True
 
+def get_password_strength(password):
+    strength = 1
+    
+    if not (password.islower() or password.isupper()):
+        strength += 1
+    if is_symbol_there(password):
+        strength += 1
+    if is_digit_there(password):
+        strength += 1
+    if is_not_from_blacklist(password):
+        strength += 4
     if len(password) > 12:
         strength += 2
     elif len(password) > 6:
         strength += 1
-
     return strength
 
 
@@ -34,4 +41,3 @@ def get_password_strength(password):
 if __name__ == '__main__':
     password = input('Enter password to check:')
     print('Your password level: %s' % get_password_strength(password))
-
