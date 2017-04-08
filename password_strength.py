fab84947295b9de3486e6c50b348a4d1bea5900b
voron434 <070809010@mail.ru>
2017-04-08 18:00:36 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -4,25 +4,36 @@ def get_password_strength(password):
     digits = '0123456789'
     password_blacklist = ['password1', '12345', 'qwerty', 'bestpassword', 'password']
     if not (password.islower() or password.isupper()):
-        strength += 2
+        strength += 1
+
     for digit in digits:
         if digit in password:
-            strength += 2
+            strength += 1
             break
+
     for symbol in symbols:
         if symbol in password:
-            strength += 2
+            strength += 1
             break
+
     for password_from_blacklist in password_blacklist:
         if password.lower() == password_from_blacklist.lower():
             break
     else:
         strength += 4
 
+    if len(password) > 18:
+        strength += 3
+    elif len(password) > 12:
+        strength += 2
+    elif len(password) > 6:
+        strength += 1
+
     return strength
 
 
 
 if __name__ == '__main__':
-    password = input()
+    password = input('Enter password to check:')
     print('Уровень вашего пароля: %s' % get_password_strength(password))
+
