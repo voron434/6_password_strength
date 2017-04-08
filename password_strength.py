646d39892a67a36be961759c4c8875888a147d6a
voron434 <070809010@mail.ru>
2017-04-08 17:57:08 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,28 @@
-
-
 def get_password_strength(password):
-    pass
+    strength = 0
+    symbols = '#$@'
+    digits = '0123456789'
+    password_blacklist = ['password1', '12345', 'qwerty', 'bestpassword', 'password']
+    if not (password.islower() or password.isupper()):
+        strength += 2
+    for digit in digits:
+        if digit in password:
+            strength += 2
+            break
+    for symbol in symbols:
+        if symbol in password:
+            strength += 2
+            break
+    for password_from_blacklist in password_blacklist:
+        if password.lower() == password_from_blacklist.lower():
+            break
+    else:
+        strength += 4
+
+    return strength
+
 
 
 if __name__ == '__main__':
-    pass
+    password = input()
+    print('Уровень вашего пароля: %s' % get_password_strength(password))
