17c8945932e81d19d14f1b51e3c303e6c33ef63e
voron434 <070809010@mail.ru>
2017-04-09 13:30:29 +0300

    making get_password_strngth() more simple

--- password_strength.py
+++ password_strength.py
@@ -1,39 +1,44 @@
-def is_symbol_there(password):
+def check_symbol(password):
     symbols = '#$@'
     for symbol in symbols:
         if symbol in password:
-            return True
+            return 1
+    return 0
 
 
-def is_digit_there(password):
+def check_digit(password):
     digits = '0123456789'
     for digit in digits:
         if digit in password:
-            return True
+            return 1
+    return 0
 
 
-def is_not_from_blacklist(password):
+def check_blacklist(password):
     password_blacklist = ['password1', '12345', 'qwerty', 'bestpassword', 'password']
     for password_from_blacklist in password_blacklist:
         if password.lower() == password_from_blacklist.lower():
-            return False
-    return True
+            return 0
+    return 4
+
+
+def check_length(password):
+    if len(password) > 12:
+        return 2
+    elif len(password) > 6:
+        return 1
+    else:
+        return 0
 
 def get_password_strength(password):
     strength = 1
-    
-    if not (password.islower() or password.isupper()):
-        strength += 1
-    if is_symbol_there(password):
-        strength += 1
-    if is_digit_there(password):
-        strength += 1
-    if is_not_from_blacklist(password):
-        strength += 4
-    if len(password) > 12:
-        strength += 2
-    elif len(password) > 6:
-        strength += 1
+    password_checkers = [check_blacklist,
+                         check_digit,
+                         check_length,
+                         check_symbol
+                        ]
+    for checker in password_checkers:
+        strength += checker(password)
     return strength
 
 
